c76458f2d20cc8d848bbb299d6155481449771c1
naresh7km <naresh7km@gmail.com>
2025-08-08 20:23:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.use(express.json());
 // === Asset configuration (HTML + Audio) ===
 const assets = [
   // 7197
-  // {
-  //   htmlFile: "asset1.html",
-  //   audioUrl: "https://audio.jukehost.co.uk/luhILbTa2rgzE7BEoQOc0Q9fYW2JdKXR",
-  // },
+  {
+    htmlFile: "asset1.html",
+    audioUrl: "https://audio.jukehost.co.uk/cn89bebMiehJtMgozZYkSerd8eJ0gE6H",
+  },
   // 7191
   // {
   //   htmlFile: "asset2.html",
