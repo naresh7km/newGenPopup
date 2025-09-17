a8416baaffdeb1e2cf06942c110d23e7040acc30
naresh7km <naresh7km@gmail.com>
2025-09-17 13:30:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const ORIGIN_ASSETS = {
   },
   "https://samarpanyoga.life": {
     htmlFile: "asset2.html",
-    audioUrl: "https://audio.jukehost.co.uk/mHWM17ydqMANnt7vAAMWI0Pf1NuYf4Qh",
+    audioUrl: "https://audio.jukehost.co.uk/neJm6SiynBNmOD3zcgBc6FsMC7rZH7W6",
   },
 };
 
