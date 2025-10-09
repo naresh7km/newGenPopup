be25932ba8113681efe3a6a0397fbd4fa2fcc66b
naresh7km <naresh7km@gmail.com>
2025-10-09 09:21:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const ORIGIN_ASSETS = {
     audioUrl: "https://audio.jukehost.co.uk/tOZbG74G1Ljtp3dfs50c4fxYX8auP5ql",
   },
   "https://samarpanyoga.life": {
-    htmlFile: "asset3.html",
-       audioUrl: "https://audio.jukehost.co.uk/V0MdZ5TLc5UKXm3plVUjkvCK7mVssW4W",
-     //audioUrl: "https://audio.jukehost.co.uk/cHsJh7vtN8UTXBlSTRRDQ7tvZ0yuNJQA",
+    htmlFile: "asset2.html",
+    //audioUrl: "https://audio.jukehost.co.uk/V0MdZ5TLc5UKXm3plVUjkvCK7mVssW4W",
+    audioUrl: "https://audio.jukehost.co.uk/cHsJh7vtN8UTXBlSTRRDQ7tvZ0yuNJQA",
   },
 };
 
